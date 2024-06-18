241575ecc6b624a1b3f6aca05633d72571ce03e9
Pradnya Mhatre <pradnyamhatre19@gmail.com>
2024-06-18 10:28:55 +0530

    static

--- django-todo/todoApp/settings.py
+++ django-todo/todoApp/settings.py
@@ -87,6 +87,6 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
